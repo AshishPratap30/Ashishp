df89c6d258b25c54fa91f8e4b6833748ece25c62
AshishPratap30 <ashishpratapsingh.101@gmail.com>
2022-05-04 15:28:04 +0530

    assignment1

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+from tkinter import N
+
+
+S="Students"
+A1=input("Enter your Name")
+A2=input("Enter your Age")
+A3=input("Enter your class")
+A4=input("Enter your address")
+A5=input("Enter your Contact Number")
+A6=input("Enter your Email ID")
+A7=input("Enter your Blood Group")
+
+
+print(S + ' , My name is ' +  A1 +'i am ' + A2 +'yeras old and i am studiying in ' + A3 +', i will live in' + A4 +' , my contact number is ' + A5 +' , my mail id is ' + A6 +' , and my blood group is ' +A7)
