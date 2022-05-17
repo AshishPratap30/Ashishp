de08912b8bdfff5c21053ff5270ba2967d0bf32d
AshishPratap30 <ashishpratapsingh.101@gmail.com>
2022-05-17 15:26:05 +0530

    Operators starting

---
+++ operators.py
@@ -0,0 +1,76 @@
+# Arithmatic Operators
+
+from cgi import print_directory
+
+
+A=12
+B=18
+
+print(A+B)
+
+print(A*B)
+
+print(B-A)
+
+print(B/A)
+
+print(A//B)
+
+print(A**9)
+
+print(B**6)
+
+print(B%A)
+
+
+# Assignment Operators
+
+C=100
+C+=10
+print(C)
+C-=20
+print(C)
+C*=25
+print(C)
+C/=2
+print(C)
+C%=2
+print(C)
+C**=8
+print(C)
+C//=2
+print(C)
+
+# Relational Operators
+
+print(A==B)
+print(A>B)
+print(A<B)
+print(A>=C)
+print(B<=C)
+print(A**B!=C)
+
+#Logical Operators
+
+print(A and B)
+print(A and B or C)
+print(not(A and B))
+
+# Bitwise Operators
+D="He is A good boy"
+E="ram loves shita"
+print(A-B)
+print(A&B)
+print(A|B)
+#print(D^E)
+print(A<<B)
+print(A>>B)
+
+
+# Membership Operators
+
+print("ram" in E)
+print("He" not in D)
+
+
+# Identity Operators
